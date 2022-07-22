14c616b70ca5631ee4233fd63158ffa4904e5469
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-07-22 10:37:49 +0530

    started working on admin form to collect product data

--- src/Components/AdminForm/AdminForm.jsx
+++ src/Components/AdminForm/AdminForm.jsx
@@ -7,16 +7,26 @@ import {
 	Input,
 } from "@chakra-ui/react";
 
+const initData = {
+	name: "",
+	brand: "",
+	price: "",
+	category: "",
+	section: "",
+	color: "",
+	images: [],
+	description: "",
+};
 export const AdminForm = () => {
 	return (
-		<Container bgGradient="linear(to-br, hotpink, darkorange)" rounded="md" p="5">
+		<Container bgGradient="linear(to-br, hotpink, orange)" rounded="md" p="5">
 			<Heading color="black" textAlign="center">
 				Admin Portal
 			</Heading>
 			<form>
-				<FormControl>
-					<FormLabel>Item Name</FormLabel>
-					<Input type="text" />
+				<FormControl my="5">
+					{/* <FormLabel>Item Name</FormLabel> */}
+					<Input type="text" placeholder="Item Name" />
 					<FormHelperText>This name will be shown to the users.</FormHelperText>
 				</FormControl>
 			</form>
